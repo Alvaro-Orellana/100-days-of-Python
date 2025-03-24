420292ac18d1c332d56654e5b433e8e740d08482
Alvaro <alvher.orellana@gmail.com>
2025-03-24 19:45:19 -0300

    refactored snake game into a snake class

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,56 +1,9 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
-CUBE_WIDTH = 20
-
-def draw_initial_snake() -> list[Turtle]:
-    snake = []
-    for i in range(3):
-        cube = Turtle("square")
-        cube.color("white")
-        cube.penup()
-        cube.backward(CUBE_WIDTH * i)
-        snake.append(cube)
-    return snake
-
-def move_snake(snake):
-    for index in range(len(snake)):
-        if index == len(snake) - 1:
-            # Move head
-            snake[index].forward(CUBE_WIDTH)
-        else:
-            # move Nth cube to Nth+1 cube position
-            next_cube_xcor = snake[index + 1].xcor()
-            next_cube_ycor = snake[index + 1].ycor()
-            snake[index].goto(next_cube_xcor, next_cube_ycor)
-
-def move_up():
-    if head.heading() == 0:
-        head.left(90)
-    if head.heading() == 180:
-        head.right(90)
-
-def move_down():
-    if head.heading() == 0:
-        head.right(90)
-    if head.heading() == 180:
-        head.left(90)
-
-def move_left():
-    if head.heading() == 90:
-        head.left(90)
-    if head.heading() == 270:
-        head.right(90)
-
-def move_right():
-    if head.heading() == 90:
-        head.right(90)
-    if head.heading() == 270:
-        head.left(90)
 
 screen = Screen()
-snake: list[Turtle] = []
-
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My snake game 🐍🐍🐍")
@@ -58,18 +11,17 @@ screen.listen()
 
 
 screen.tracer(0)
-snake = draw_initial_snake()
+snake = Snake()
 screen.update()
-head = snake[len(snake) - 1]
 
-screen.onkey(key="Up", fun=move_up)
-screen.onkey(key="Down", fun=move_down)
-screen.onkey(key="Left", fun=move_left)
-screen.onkey(key="Right", fun=move_right)
+screen.onkey(key="Up", fun=snake.move_up)
+screen.onkey(key="Down", fun=snake.move_down)
+screen.onkey(key="Left", fun=snake.move_left)
+screen.onkey(key="Right", fun=snake.move_right)
 
 game_is_over = False
 while not game_is_over:
-    move_snake(snake)
+    snake.move()
     screen.update()
     time.sleep(0.1)
 
--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,6 +1,59 @@
 from turtle import Turtle
 
 class Snake:
+
     CUBE_WIDTH = 20
     INITIAL_NUMBER_OF_CUBES = 3
+    # turtle's module directions in degrees
+    directions = { "east": 0, "north": 90, "west": 180, "south": 270 }
 
+    def __init__(self, color: str="white"):
+        self.snake: list[Turtle] = []
+
+        # create initial snake
+        for i in range(self.INITIAL_NUMBER_OF_CUBES):
+            cube = Turtle("square")
+            cube.color(color)
+            cube.penup()
+            #puts the cubes horizontally next to each other
+            cube.backward(self.CUBE_WIDTH * i)
+            self.snake.append(cube)
+
+        self.head = self.snake[len(self.snake) - 1]
+
+    def move(self):
+        """first moves tail to the next cube's position and then moves the head"""
+
+        tail_length = len(self.snake) - 1
+        # move tail
+        for index in range(tail_length):
+            # move Nth cube to Nth+1 cube position
+            next_xcor = self.snake[index + 1].xcor()
+            next_ycor = self.snake[index + 1].ycor()
+            self.snake[index].goto(next_xcor, next_ycor)
+
+        # move head
+        self.head.forward(self.CUBE_WIDTH)
+
+    def move_up(self):
+        heading = self.head.heading()
+        if heading == self.directions["east"] or heading == self.directions["west"]:
+            self.turn("north")
+
+    def move_down(self):
+        heading = self.head.heading()
+        if heading == self.directions["east"] or heading == self.directions["west"]:
+            self.turn("south")
+
+    def move_left(self):
+        heading = self.head.heading()
+        if heading == self.directions["north"] or heading == self.directions["south"]:
+            self.turn("west")
+
+    def move_right(self):
+        heading = self.head.heading()
+        if heading == self.directions["north"] or heading == self.directions["south"]:
+            self.turn("east")
+
+    def turn(self, direction: str):
+        self.head.setheading(self.directions[direction])
