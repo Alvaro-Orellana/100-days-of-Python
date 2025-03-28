88072d1dd13aef498a335e72fbf65fbe0cfdfeb1
Alvaro <alvher.orellana@gmail.com>
2025-03-28 12:49:31 -0300

    Pong game working
    Theres only a bug where the scoreboard doesnt appear right some of the time

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -1,36 +1,61 @@
 import random
-import time
-import turtle
+from turtle import Turtle
 
-class Ball(turtle.Turtle):
-
-    def __init__(self, ball_speed: int):
+class Ball(Turtle):
+    def __init__(self, initial_speed: int):
         super().__init__()
-        self.ball_speed = ball_speed
         self.shape("circle")
         self.color("white")
         self.penup()
-
-        self.dx = 1 # x direction either 1 for left or -1 for right
-        self.dy = 1 # y direction either 1 for up or -1 for down
+        self.initial_speed = initial_speed
+        self.dx = initial_speed
+        self.dy = initial_speed
         self.reset()
 
     def move(self):
-        new_x = self.xcor() + self.ball_speed * self.dx
-        new_y = self.ycor() + self.ball_speed * self.dy
+        new_x = self.xcor() + self.dx
+        new_y = self.ycor() + self.dy
         self.goto(new_x, new_y)
 
-    def bounce_x(self):
-       self.dx *= -1
-
-    def bounce_y(self):
-        self.dy *= -1
-
-    def is_going_left(self) -> bool:
-        return self.dx == 1
+    def bounce_x(self): self.dx *= -1
+    def bounce_y(self): self.dy *= -1
+    def is_going_left(self) -> bool: return self.dx < 0
+    def increase_speed(self):
+        d_velocity = -0.5 if self.dx < 0 else 0.5
+        self.dx += d_velocity
 
     def reset(self):
         self.goto(0, 0)
-        self.dx = random.choice([-1, 1])
-        self.dy = random.choice([-1, 1])
-        time.sleep(0.8)
+        self.dx = self.initial_speed * random.choice([-1, 1])
+        self.dy = self.initial_speed * random.choice([-1, 1])
+
+
+class Paddle(Turtle):
+    def __init__(self, pad_length: int=4, velocity: int=18):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.setheading(90) # point up
+        self.shapesize(stretch_len=pad_length)
+        self.velocity = velocity
+
+    def move_up(self): self.forward(self.velocity)
+    def move_down(self): self.backward(self.velocity)
+    def touches(self, object: Turtle) -> bool: return self.distance(object) < 20
+
+
+class Scoreboard(Turtle):
+    FONT = ('Arial', 30, 'normal')
+
+    def __init__(self):
+        super().__init__()
+        self.left_paddle_score = 0
+        self.right_paddle_score = 0
+        self.color("white")
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"{self.left_paddle_score}\t\t{self.right_paddle_score}", align="center", font=self.FONT)
--- Day 22/main.py
+++ Day 22/main.py
@@ -1,25 +1,23 @@
+import time
 from turtle import Turtle, Screen
-from paddle import Paddle
-from ball import Ball
-from scoreboard import Scoreboard
+from ball import Ball, Paddle, Scoreboard
 
 BALL_SPEED = 2
 WIDTH = 800
 HEIGHT = 600
-
 MIN_X_COORDINATE, MAX_X_COORDINATE = -WIDTH // 2, WIDTH // 2
 MIN_Y_COORDINATE, MAX_Y_COORDINATE = -HEIGHT // 2, HEIGHT // 2
 
 def position_paddles():
-    left_paddle.move_xcoor(MIN_X_COORDINATE + 40)
-    right_paddle.move_xcoor(MAX_X_COORDINATE - 40)
+    left_paddle.setx(MIN_X_COORDINATE + 40)
+    right_paddle.setx(MAX_X_COORDINATE - 40)
 
 def draw_middle_line():
     t = Turtle()
     t.penup()
     t.color("white")
     t.goto(0, MAX_Y_COORDINATE) # Start at the top center
-    t.setheading(270)  # Face downward
+    t.setheading(270)  # Face down
 
     for _ in range(HEIGHT // 20):
         t.pendown()
@@ -34,37 +32,37 @@ def ball_touches_up_or_down_wall(ball: Ball) -> bool:
     return ball.ycor() >= MAX_Y_COORDINATE or ball.ycor() <= MIN_Y_COORDINATE
 
 screen = Screen()
-canvas = screen.getcanvas()
 left_paddle = Paddle()
 right_paddle = Paddle()
 ball = Ball(BALL_SPEED)
 scoreboard = Scoreboard()
 
-scoreboard.goto(0, MAX_X_COORDINATE - 40)
-
+# setup screen
 screen.setup(WIDTH, HEIGHT)
 screen.bgcolor("black")
+screen.title("Pong")
 screen.tracer(0)
-screen.onkey(key="q", fun=left_paddle.move_up)
-screen.onkey(key="a", fun=left_paddle.move_down)
+screen.onkey(key="w", fun=left_paddle.move_up)
+screen.onkey(key="s", fun=left_paddle.move_down)
 screen.onkey(key="Up", fun=right_paddle.move_up)
 screen.onkey(key="Down", fun=right_paddle.move_down)
 screen.listen()
 
+scoreboard.goto(0, MAX_Y_COORDINATE - 20)
 position_paddles()
 draw_middle_line()
 
 while True:
     ball.move()
+    screen.update()
 
-    paddle = right_paddle if ball.is_going_left() else left_paddle
+    paddle = left_paddle if ball.is_going_left() else right_paddle
     if paddle.touches(ball):
         ball.bounce_x()
-        continue
+        ball.increase_speed()
 
     if ball_touches_up_or_down_wall(ball):
         ball.bounce_y()
-        continue
 
     if ball_touches_left_or_right_wall(ball):
         if ball.is_going_left():
@@ -72,7 +70,5 @@ while True:
         else:
             scoreboard.right_paddle_score += 1
         ball.reset()
-        scoreboard.show_score()
-
-    screen.update()
-
+        scoreboard.update_score()
+        time.sleep(0.8)
