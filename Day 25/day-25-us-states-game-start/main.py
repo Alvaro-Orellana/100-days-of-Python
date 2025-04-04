9bcdc30eb5c00abefb4a7e85856776093919be7a
Alvaro <alvher.orellana@gmail.com>
2025-04-04 13:56:26 -0300

    day 25 done

---
+++ Day 25/day-25-us-states-game-start/main.py
@@ -0,0 +1,32 @@
+import turtle as t
+import pandas
+
+# setup screen
+screen = t.Screen()
+screen.title("US States Guessing Game")
+screen.bgpic("blank_states_img.gif")
+
+# setup pen
+pen = t.Turtle()
+pen.hideturtle()
+pen.penup()
+
+# load states data
+states_table = pandas.read_csv("50_states.csv")
+remaining_states_names = states_table.state.to_list()
+
+while remaining_states_names:
+    correct_guesses = 50 - len(remaining_states_names)
+    guess = t.textinput(f"{correct_guesses}/50 States Correct", "What's another state name?").title()
+
+    if guess in remaining_states_names:
+        state_row = states_table[states_table.state == guess]
+        pen.goto(state_row.x.item(), state_row.y.item())
+        pen.write(guess, align="center")
+        remaining_states_names.remove(guess)
+
+    if guess == "Exit":
+        break
+
+pandas.DataFrame(remaining_states_names, columns=["States"]).to_csv("states_to_learn.csv")
+screen.mainloop()
