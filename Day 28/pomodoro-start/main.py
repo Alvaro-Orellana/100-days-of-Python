92df5f7ee9012e927c902236321a14add9b40eff
Alvaro <alvher.orellana@gmail.com>
2025-04-08 00:15:37 -0400

    UI for day 28 almost done

---
+++ Day 28/pomodoro-start/main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def start_pressed():
+    pass
+
+def reset_pressed():
+    pass
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_image = tk.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+
+timer_label = tk.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+start_button = tk.Button(text="Start", command=start_pressed)
+reset_button = tk.Button(text="Reset", command=reset_pressed, fg=YELLOW, bg=YELLOW)
+check_label = tk.Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+
+timer_label.grid(row=0, column=1)
+canvas.grid(row=1, column=1)
+start_button.grid(row=2, column=0)
+reset_button.grid(row=2, column=2)
+check_label.grid(row=3, column=1)
+
+window.mainloop()
