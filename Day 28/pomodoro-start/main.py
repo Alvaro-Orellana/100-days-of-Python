64d1b70b350c081eba754273fd66d024fac6ec6c
Alvaro <alvher.orellana@gmail.com>
2025-04-08 13:25:21 -0400

    pomodoro timer countdown working. but theres a problems with concurrency

--- Day 28/pomodoro-start/main.py
+++ Day 28/pomodoro-start/main.py
@@ -1,13 +1,14 @@
 import tkinter as tk
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MINUTES = 25
+SHORT_BREAK_MINUTES = 5
+LONG_BREAK_MINUTES = 20
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def start_pressed():
@@ -16,10 +17,31 @@ def start_pressed():
 def reset_pressed():
     pass
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def timer():
+    count_down(seconds=WORK_MINUTES * 60)
+    count_down(seconds=SHORT_BREAK_MINUTES * 60)
+    #window.after(WORK_MINUTES*60, count_down, SHORT_BREAK_MINUTES * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    # update checks
+    check_label_text = check_label.cget("text")
+    check_label_text += "✓\n"
+    check_label.config(text=check_label_text)
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(seconds):
+    if seconds >= 0:
+        canvas.itemconfigure(timer_text, text=format_seconds(seconds)) # update label text
+        window.after(1000, count_down, seconds - 1)
+
+
+def format_seconds(seconds: int) -> str:
+    minutes_remaining, seconds_remaining = divmod(seconds, 60)
+
+    minutes_remaining = str(minutes_remaining) if minutes_remaining > 9 else "0" + str(minutes_remaining)
+    seconds_remaining = str(seconds_remaining) if seconds_remaining > 9 else "0" + str(seconds_remaining)
+
+    return minutes_remaining + ":" + seconds_remaining
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Pomodoro")
@@ -28,17 +50,18 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
 
 timer_label = tk.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
-start_button = tk.Button(text="Start", command=start_pressed)
-reset_button = tk.Button(text="Reset", command=reset_pressed, fg=YELLOW, bg=YELLOW)
-check_label = tk.Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+start_button = tk.Button(text="Start", command=start_pressed, highlightthickness=0)
+reset_button = tk.Button(text="Reset", command=reset_pressed, highlightthickness=0)
+check_label = tk.Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
 
-timer_label.grid(row=0, column=1)
 canvas.grid(row=1, column=1)
+timer_label.grid(row=0, column=1)
 start_button.grid(row=2, column=0)
 reset_button.grid(row=2, column=2)
 check_label.grid(row=3, column=1)
 
+timer()
 window.mainloop()
