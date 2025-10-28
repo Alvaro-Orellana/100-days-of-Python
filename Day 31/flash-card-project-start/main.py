6b2031b9cbad2f8917121eea69dd8efad42afe21
Alvaro <alvher.orellana@gmail.com>
2025-10-28 18:38:07 -0300

    project 31 finished. only needs a bit of polishing

--- Day 31/flash-card-project-start/main.py
+++ Day 31/flash-card-project-start/main.py
@@ -1,52 +1,78 @@
 from tkinter import *
 import pandas
 import random
-import time
 
 BACKGROUND_COLOR = "#B1DDC6"
-words = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
-current_words = random.choice(words)
+words = []
+current_card = {}
 
-def get_next_word():
-    global image; image = PhotoImage(file="images/card_back.png")
-    global current_words; current_words = random.choice(words)
+def load_words():
+    global words
+    try:
+        with open("data/words_to_learn.csv", 'r') as file:
+            words = pandas.read_csv(file).to_dict(orient="records")
 
+    except FileNotFoundError:
+        words = pandas.read_csv("data/french_words.csv").to_dict(orient="records")[1:]
+
+    print(words)
+
+def button_pressed(button: str | None = None):
+    global flip_timer, current_card
+
+    # reset timer
+    window.after_cancel(flip_timer)
+    flip_timer = window.after(3000, flip_card)
+
+    if not words:
+        canvas.itemconfig(language_label, text='No hay mas cartas', fill='black')
+        canvas.itemconfig(word_label, text="Las memorizaste todas", fill='black')
+        return
+    current_card = random.choice(words)
+
+    # update ui
     canvas.itemconfig(language_label, text='French', fill='black')
-    canvas.itemconfig(word_label, text=current_words["french_word"], fill='black')
+    canvas.itemconfig(word_label, text=current_card["french_word"], fill='black')
+    canvas.itemconfig(card_background, image=front_image)
+
+    # save words
+    with open("data/words_to_learn.csv", 'w') as file:
+        if button == 'right_button':  # save all words except the current word, the user alredy knows its translation
+            words.remove(current_card)
+            pandas.DataFrame(words).to_csv(file, index=False)
+        elif button == 'wrong_button':  # the user doesn't know the current word, so we save that and all the others
+            pandas.DataFrame(words).to_csv(file, index=False)
+            words.remove(current_card)
+
 
 def flip_card():
-    global image
-
-    image = PhotoImage(file="images/card_back.png")
+    canvas.itemconfig(card_background, image=back_image)
     canvas.itemconfig(language_label, text='English', fill='white')
-    canvas.itemconfig(word_label, text=current_words["english_translation"], fill='white')
-
+    canvas.itemconfig(word_label, text=current_card["english_translation"], fill='white')
 
 window = Tk()
 window.title("Flash Card 2")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 268, image=image)
+
+front_image = PhotoImage(file="images/card_front.png")
+back_image = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 268, image=front_image)
 
 language_label = canvas.create_text(400, 150, text="", fill="black", font=("ariel", 40, "italic"))
 word_label = canvas.create_text(400, 263, text="", fill="black", font=("ariel", 60, "bold"))
-#language_label = Label(text="", font=("ariel", 40, "italic"))
-#word_label = Label(text="", font=("ariel", 60, "bold"))
 
 right_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_image, highlightthickness=0, command=get_next_word)
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=lambda: window.after(1000, flip_card))
+right_button = Button(image=right_image, highlightthickness=0, command=lambda : button_pressed('right_button'))
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=lambda : button_pressed('wrong_button'))
 
-#language_label.place(x=320, y=150)
-#word_label.place(x=320, y=263)
 canvas.grid(row=0, column=0, columnspan=2)
 wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
 
-
-
-get_next_word()
+load_words()
+button_pressed()
 window.mainloop()
