bf4a2f042b755393aa565a93c0aa824cec701e37
Alvaro <alvher.orellana@gmail.com>
2025-11-01 22:13:25 -0300

    refactor proyecto dia 32

--- Day 32/birthday-wisher-extrahard-start/main.py
+++ Day 32/birthday-wisher-extrahard-start/main.py
@@ -8,9 +8,28 @@ import csv  ##################### Extra Hard Starting Project ##################
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
-import datetime
+import datetime as dt
 import pandas as pd
+import random
+import smtplib
 
-pd.read_csv('birthdays.csv')
+def send_email(to_email:str, message:str):
+    FROM_EMAIL = "testforpythoncourse@gmail.com"
+    PASSWORD = "kagf zprj ctit xjyw"
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(FROM_EMAIL, PASSWORD)
+        connection.sendmail(FROM_EMAIL, to_email, message)
+
+today = dt.datetime.today()
+birthdays_table = pd.read_csv('birthdays.csv')
+
+for _, row in birthdays_table.iterrows():
+    if (today.month, today.day) == (row.month, row.day):
+        letter_file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+        with open(letter_file_path) as letter:
+            message = letter.read().replace("[NAME]", row['name'])
+            send_email(row.email, message)
 
 
