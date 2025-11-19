1b8590cbe25e74c9ae1b6f5852bb0a9214142ffa
Alvaro <alvher.orellana@gmail.com>
2025-11-19 19:45:13 -0300

    dia 36 terminado

--- Day 36/stock-news-extrahard-start/main.py
+++ Day 36/stock-news-extrahard-start/main.py
@@ -3,8 +3,8 @@ import requests
 from dataclasses import dataclass
 from twilio.rest import Client
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+STOCK = "AMD"
+COMPANY_NAME = "AMD"
 DESTINATION_NUMBER = '+56928121317'
 
 @dataclass
@@ -19,9 +19,6 @@ def get_stock_percent_change(stock_sticker: str) -> float:
     params = {"apikey":alpha_avantage_api_key, "function":"GLOBAL_QUOTE", "symbol":stock_sticker,}
     response = requests.get("https://www.alphavantage.co/query", params=params)
     response.raise_for_status()
-
-    #TODO: check if this api returns negative percentages or only positive ones
-    "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=TSLA&apikey=BE0TAZVO3KT9P0TK"
 
     percent_change = response.json()["Global Quote"]["10. change percent"]
     percent_change = percent_change.strip('%')
@@ -40,12 +37,16 @@ def get_company_news_articles(company_name: str) -> list[NewsArticle]:
 
 if __name__ == '__main__':
     company_stock_price_change = get_stock_percent_change(STOCK)
-    if company_stock_price_change > 5:
+    if abs(company_stock_price_change) > 0.1:
         # Send a seperate message with the percentage change and each article's title and description to your phone number.
         account_sid, auth_token = os.environ.get('TWILIO_ACCOUNT_SID'), os.environ.get("TWILIO_AUTH_TOKEN")
         twilio_client = Client(account_sid, auth_token)
         symbol = "🔺" if company_stock_price_change > 0 else "🔻" if company_stock_price_change < 0 else ""
 
+        print("got messages list and are about to send thme")
         for article in get_company_news_articles(COMPANY_NAME):
-            message = f"{STOCK}: {symbol}%\nHeadline: {article.title}\nBrief: {article.description}"
-            twilio_client.messages.create(from_='+19522436053', body=message, to=DESTINATION_NUMBER)
+            message = f"{STOCK}: {symbol}{company_stock_price_change}%\nHeadline: {article.title}\n" # aca falta agregar el article.description, lo saque porque twilio daba error por mensaej muy alrgo
+            message = twilio_client.messages.create(from_='+19522436053', body=message, to=DESTINATION_NUMBER)
+            print(message.sid)
+    else:
+        print("no significant price change")
