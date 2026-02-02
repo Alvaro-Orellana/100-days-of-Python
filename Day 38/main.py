00a57d47ee2879a40eebc6c4822bbd935eb6e41f
Alvaro <alvher.orellana@gmail.com>
2026-02-02 18:02:14 -0300

    DIA 38 TERMINADOOOOO
    TENGO QUE PONERME MAS LAS PILAS
    
    EL ULTIMO COMMIT LO HICE HACE 2 MESES Y PARA TERMINAR EL PROYECTO ERA SOLO HACER UNAS POCAS LINEAS DE CODIGO QUE NI ERAN COMPLICADAS, SOLO SACAR DATOS DE UNA RESPEUSTA ARMAR UN BODY CONE SOS DATOS Y ENVIAR UN POST A UN NUEVO ENDPOINT

--- Day 38/main.py
+++ Day 38/main.py
@@ -1,24 +1,50 @@
 import requests
+import datetime
+import os
 
-APP_KEY = 'nix_live_9x1aMPoW9YIJcWaqYXJoCSwyUiwM3TCO'
-APP_ID = 'app_209039621b3d48cd913938e9'
+
+APP_ID = os.environ.get("APP_ID")
+APP_KEY = os.environ.get("APP_KEY")
+SHEET_API_ENDPOINT = os.environ.get("SHEET_API_ENDPOINT")
+SHEETY_ACCESS_TOKEN = os.environ.get("SHEETY_ACCESS_TOKEN")
+
 BASE_URL = 'https://app.100daysofpython.dev'
-
-endpoint = '/v1/nutrition/natural/exercise'
+ENDPOINT = '/v1/nutrition/natural/exercise'
 
 headers = {
     'x-app-id': APP_ID,
     'x-app-key': APP_KEY
 }
-
-exersise = input("Tell me which exercises you did today: ")
+exercise = input("Tell me which exercises you did today: ")
 body = {
-    "query": exersise
+    "query": exercise
 }
 
-response = requests.post(BASE_URL+endpoint, headers=headers, json=body)
+response = requests.post(BASE_URL + ENDPOINT, headers=headers, json=body)
 response.raise_for_status()
-
 data = response.json()
-
 print(data['exercises'][0])
+
+duration = data['exercises'][0]['duration_min']
+exercise = data['exercises'][0]['name']
+calories = data['exercises'][0]['nf_calories']
+ahora = datetime.datetime.now()
+
+
+sheety_body = {
+    'workout' : {
+        'Date' : ahora.strftime("%d/%m/%Y"),
+        'Time' : ahora.strftime("%H:%M:%S"),
+        'Exercise' : exercise,
+        'Duration' : duration,
+        'Calories' : calories,
+    }
+}
+header2 = {
+    'Authorization': f"Authorization: Bearer {SHEETY_ACCESS_TOKEN}",
+    'Content-Type': 'application/json',
+}
+
+response = requests.post(SHEET_API_ENDPOINT, headers=header2, json=sheety_body)
+response.raise_for_status()
+print(response.json())
